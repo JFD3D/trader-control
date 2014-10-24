496dd7fe773a17c5e7078df168cffa9ed4f60910
rorygreig <rorygreig@gmail.com>
2014-10-24 11:54:10 +0100

    check balance tests passing

--- test/testCheckBalance.js
+++ test/testCheckBalance.js
@@ -1,15 +1,14 @@
 var assert = require('chai').assert;
+var async = require('async');
 var checkBalance = require('../lib/checkBalance.js');
-var traderUtils = require('../lib/traderDBUtils.js');
+var TraderUtils = require('../lib/traderDBUtils.js');
 
 var valueDelta = 0.00000001; //acceptable error for numerical quantities (accurate to 1 satoshi)
-var testUID = 1000;
+var testUserID;
 
-describe('test check balance functions',function(){
+describe('test check balance non async functions',function(){
 
-  //setup - create trader and and some loans on Coinfloor and btcchina
-
-  it('test getAccountPresentValue()',function(){
+  it('test getAccountPresentValue()', function(){
 
     var XBTBalance = 0.05;
     var GBPBalance = 10;
@@ -32,34 +31,97 @@ describe('test check balance functions',function(){
     assert.closeTo(actual, expected, valueDelta, 'minimum present value calculated is not equal to expected');
   });
 
-  it('test getOutstandingLoanBalance()',function(){
-    var expected = 2.1;
+});
 
-    var actual = checkBalance.getOutstandingLoanBalance(testUID, "coinfloor");
+describe('test check balance async', function(){
 
-    assert.closeTo(actual, expected, valueDelta, 'minimum present value calculated is not equal to expected');
+  before(function(done){
+    TraderUtils.createTraderInDB("test_user", '1000', 'password', 'apikeytest123', function(result){
+      testUserID = result;
+      //create some test loans for test user
+      async.parallel([
+          TraderUtils.addLoanForTrader(testUserID, 0.1, "coinfloor", "2014-10-22 16:30:38", function(){;}),
+          TraderUtils.addLoanForTrader(testUserID, 0.23, "coinfloor", "2014-10-22 16:30:38", function(){;}),
+          TraderUtils.addLoanForTrader(testUserID, 0.4, "BTCChina", "2014-10-22 16:30:38", function(){;})
+        ], done());
+    });
   });
 
-  it('test isAboveMaintenanceValue(): expected true',function(){
-    var XBTBalance = 1;
-    var GBPBalance = 500;
+  after(function(){
+    // call delete functions
+    async.series([
+      TraderUtils.deleteAllLoans(testUserID, function(){;}),
+      TraderUtils.deleteTraderFromDB(testUserID, function(){;})
+    ], function(result){console.log(result);});
+  });
+
+  describe('greater than minimum value',function(){
+    //total loans on coinfloor = 0.33XBT
+    //therefore minimum value = 0.33*(1+0.2) = 0.396 (because of maintenance req)
+    var XBTBalance = 0.25;
     var XBTAskPrice = 240.0;
+    var GBPBalance = 0.15*XBTAskPrice;  //should equal 0.15 XBT, so total = 0.40 XBT (> total)
 
-    var actual = checkBalance.isAboveMaintenanceValue(XBTBalance, GBPBalance, XBTAskPrice, testUID, "coinfloor");
+    var expected = true;
+    var actual = false
 
-    assert.isTrue(actual, 'present value is above maintenance value');
+    before(function(done){
+      checkBalance.isAboveMaintenanceValue(XBTBalance, GBPBalance, XBTAskPrice, testUserID, "coinfloor", function(result){
+        actual = result;
+        console.log(result);
+        done();
+      });
+    })
+
+    it('should return true', function(){
+      assert.isTrue(actual, 'present value is above maintenance value');
+    });
   });
 
-  it('test isAboveMaintenanceValue(): expected false',function(){
-    var XBTBalance = 0.05;
-    var GBPBalance = 10;
+  describe('lower than minimum value',function(){
+    //total loans on coinfloor = 0.33XBT
+    //therefore minimum value = 0.33*(1+0.2) = 0.396 (because of maintenance req)
+    var XBTBalance = 0.25;
     var XBTAskPrice = 240.0;
+    var GBPBalance = 0.14*XBTAskPrice;  //should equal 0.14 XBT, so total = 0.39 XBT (< total)
 
-    var actual = checkBalance.isAboveMaintenanceValue(XBTBalance, GBPBalance, XBTAskPrice, testUID, "coinfloor");
+    var expected = true;
+    var actual = false
 
-    assert.isFalse(actual, 'present value is no above maintenance value');
+    before(function(done){
+      checkBalance.isAboveMaintenanceValue(XBTBalance, GBPBalance, XBTAskPrice, testUserID, "coinfloor", function(result){
+        actual = result;
+        console.log(result);
+        done();
+      });
+    })
+
+    it('should return false', function(){
+      assert.isFalse(actual, 'present value is below maintenance value');
+    });
   });
 
-  //teardown - delete trader created in setup
+  describe('equal to minimum value',function(){
+    //total loans on coinfloor = 0.33XBT
+    //therefore minimum value = 0.33*(1+0.2) = 0.396 (because of maintenance req)
+    var XBTBalance = 0.25;
+    var XBTAskPrice = 240.0;
+    var GBPBalance = 0.146*XBTAskPrice;  //should equal 0.15 XBT, so total = 0.396 XBT (== total)
+
+    var expected = true;
+    var actual = false
+
+    before(function(done){
+      checkBalance.isAboveMaintenanceValue(XBTBalance, GBPBalance, XBTAskPrice, testUserID, "coinfloor", function(result){
+        actual = result;
+        console.log(result);
+        done();
+      });
+    })
+
+    it('should return true', function(){
+      assert.isTrue(actual, 'present value is equal maintenance value');
+    });
+  });
 
 });
